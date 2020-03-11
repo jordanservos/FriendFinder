83c5d724dc04df5123b6ef34fc36e784efdd644c
Jordan Servos <jservos11@yahoo.com>
2020-03-11 09:00:51 -0600

    routes, data and html updates

---
+++ app/data/friends.js
@@ -0,0 +1,134 @@
+var friendsInfo = [
+
+    {
+        "name": "Ahmed",
+        "photo": "https://api.time.com/wp-content/uploads/2017/12/joey-degrandis-hsam-memory.jpg",
+        "scores": [
+            5,
+            1,
+            3,
+            4,
+            5,
+            5,
+            2,
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Joe",
+        "photo": "https://resources.stuff.co.nz/content/dam/images/1/5/f/s/8/i/image.related.StuffLandscapeSixteenByNine.710x400.15v4gk.png/1440553039035.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Sue",
+        "photo": "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
+        "scores": [
+            3,
+            1,
+            5,
+            4,
+            5,
+            1,
+            2,
+            4,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "Mary",
+        "photo": "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "scores": [
+            1,
+            1,
+            4,
+            2,
+            3,
+            1,
+            2,
+            5,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Rick",
+        "photo": "https://www.abajournal.com/images/main_images/harley_blakeman500px.png",
+        "scores": [
+            2,
+            1,
+            2,
+            5,
+            2,
+            1,
+            2,
+            5,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "Jesse",
+        "photo": "https://digital-photography-school.com/wp-content/uploads/old/travel-people.jpg",
+        "scores": [
+            3,
+            1,
+            4,
+            4,
+            2,
+            5,
+            2,
+            1,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Erica",
+        "photo": "https://about.fb.com/wp-content/uploads/2019/11/CompanyInfo_OurCulture_Grace_Image-1.jpg?fit=890%2C668",
+        "scores": [
+            5,
+            5,
+            2,
+            4,
+            1,
+            1,
+            2,
+            3,
+            1,
+            1
+        ]
+    }, {
+        "name": "Steve",
+        "photo": "https://jooinn.com/images/people-9.jpg",
+        "scores": [
+            3,
+            3,
+            2,
+            1,
+            5,
+            1,
+            5,
+            2,
+            4,
+            1
+        ]
+    },
+
+
+]
+
+module.exports = friendsInfo;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,80 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsInfo = require("../data/friends.js");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function(req, res) {
+        // console.log("This is working");
+        res.json(friendsInfo);
+
+    });
+
+
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+
+    app.post("/api/friends", function(req, res) {
+
+        var friendMatch = {
+            name: "",
+            photo: "",
+            scores: "",
+            scoreDifference: 100
+
+
+        };
+
+        var totalDiff = 0;
+
+        for (var i = 0; i < friendsInfo.length; i++) {
+            totalDiff = 0;
+
+            for (var j = 0; j < friendsInfo[i].score; j++) {
+                console.log("This is working" + friendsInfo[i].score);
+
+
+                totalDiff += Math.abs(parseInt(req.body.scores[j]) - parseInt(friendsInfo[i].scores[j]));
+
+                if (totalDiff <= friendMatch.scoreDifference) {
+                    friendMatch.name = friendsInfo[i].name;
+                    friendsMatch.photo = friendsInfo[i].photo;
+                    friendMatch.scoreDifference = totalDiff;
+                }
+
+            }
+
+
+
+        };
+
+        friendsInfo.push(req.body);
+        res.json(friendMatch);
+        console.log("Your best match is " + friendMatch)
+
+
+
+    });
+
+}
