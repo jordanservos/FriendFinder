585385a030a9472f9b0c37f0c6ff6bf642d1d679
Jordan Servos <jservos11@yahoo.com>
2020-03-11 10:51:17 -0600

    Fully styled

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -19,6 +19,11 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
+    app.get("/style.css", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/style.css"));
+    });
+
+
     app.use(function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
